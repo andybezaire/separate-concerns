e9d2028121cdd7008c0cbd2f2ffb764b5690d84b
andy bezaire <andy.bezaire@gmail.com>
2022-01-20 20:20:20 +0200

    keep that text so the test passes

--- src/SimpleView.tsx
+++ src/SimpleView.tsx
@@ -10,11 +10,13 @@ function SimpleView({ title }: SimpleViewProps) {
     return (
         <div className="App" >
             <header className="App-header" >
+                <p>Learn React!</p>
                 <img src={logo} className="App-logo" alt="logo" />
 
                 <p>
                     {title}
                 </p>
+                <button onClick={() => { }}>Click me</button>
 
             </header>
         </div>
